ab7f97308d6484bad007e929e5c2c7764ce11e44
xingcbrew <xingcbrew@gmail.com>
2023-08-03 14:48:19 -0400

    redo script to create external csv to attach

--- scripts/ento_metadata/ento_hh_le_first_visit_list.R
+++ scripts/ento_metadata/ento_hh_le_first_visit_list.R
@@ -2,20 +2,29 @@
 library(dplyr)
 library(tidyr)
 
-setwd("bk/scripts/ento_metadata")
-
 hhfirst <- read.csv("../Downloads/entohhfirstvisitke.csv")
 lefirst <- read.csv("../Downloads/entolefirstvisitke.csv")
 
-hhfirst <- hhfirst %>% select(hhid) %>% 
-  mutate(hhid = ifelse(nchar(hhid) == 4, paste0("0", hhid), hhid)) %>%
-  arrange(hhfirst)
+setwd("bk/scripts/ento_metadata")
 
-hhfirst$hhid <- sprintf("%s", hhfirst$hhid)
+hhlist <- hhfirst %>%
+  mutate(hhid = ifelse(nchar(hhid) == 4, paste0("0", hhid), hhid)) %>% # add leading 0 to 4-digit hhids 
+  arrange(hhid) %>% 
+  mutate(name = as.character(hhid),   # Convert 'hhid' to character and store in 'name'
+         label = name,                 # Duplicate 'name' into 'label'
+         list_name = "hhid") %>%      # Create 'list_name' column with value "hhid"
+ select(list_name, name, label) 
+ hhlist$name <- sprintf("%s", hhlist$name)
+ hhlist$label<- sprintf("%s", hhlist$label)
 
-lefirst <- lefirst %>% select(leid) %>%
-  arrange(lefirst)
 
-write.table(hhfirst, "first_visit_hhids.xlsx", row.names = FALSE, col.names = FALSE)
-write.table(lefirst, "first_visit_leids.xlsx", row.names = FALSE, col.names = FALSE)
+lelist <- lefirst %>% select(leid) %>%
+  arrange(leid) %>% 
+   mutate(name = leid,   # Convert 'hhid' to character and store in 'name'
+         label = name,                 # Duplicate 'name' into 'label'
+         list_name = "leid") %>%      # Create 'list_name' column with value "hhid"
+ select(list_name, name, label) 
+
+write.table(hhlist, "first_visit_hhids.csv", sep = ",", row.names = FALSE)
+write.table(lelist, "first_visit_leids.csv", sep = ",", row.names = FALSE)
 
