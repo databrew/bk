c474a90f34a025db9cc4ff112e549b40c15bf0bb
Joe Brew <joebrew@gmail.com>
2023-10-10 09:44:02 +0200

    minor ento metadata date changes

--- scripts/ento_metadata/generate_ento_metadata.R
+++ scripts/ento_metadata/generate_ento_metadata.R
@@ -121,8 +121,8 @@ out <-
 filter_by_dates <- FALSE
 if(filter_by_dates){
   out <- out %>% filter(
-    todays_date >= as.Date('2023-09-12'),
-    todays_date <= as.Date('2023-09-14')
+    todays_date >= as.Date('2023-10-03'),
+    todays_date <= as.Date('2023-10-05')
   )
 }
 
