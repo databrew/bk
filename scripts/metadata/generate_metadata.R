0afe8db014ebf644c0822f8c2cd028a4d886fef8
Joe Brew <joebrew@gmail.com>
2023-12-19 13:34:21 +0100

    not using caches vcs data division

--- scripts/metadata/generate_metadata.R
+++ scripts/metadata/generate_metadata.R
@@ -1078,7 +1078,7 @@ reason_out <- individuals %>%
 # this instruction has been provided at https://bohemiakenya.slack.com/archives/C042KSRLYUA/p1695366280214239
 # Load the spreadsheet from the project
 
-if(TRUE){
+if(FALSE){
   # Set the above boolean as follows
   # As of visit 2, just use previous division = TRUE
   # As of visit 2 onwards, use balanced division = FALSE
